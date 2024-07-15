5818d94bd62b756dd1f95a05f65d4fbd3601a66c
satvik805 <s.sharma.51@student.scu.edu.au>
2024-07-15 23:50:34 +1000

    calculate highest and lowest
    program wil be able to calculate highest and lowest marks

--- assesment1/assignment1.java
+++ assesment1/assignment1.java
@@ -17,7 +17,7 @@ public class assignment1
        System.out.println(name);
        System.out.println("Name of the assignment : " + name);
        System.out.println("Marks of students : " + marks);
-       int[] highestlowest = computeminmax(marks); // compute highest and lowest marks out of 30 students
+       float[] highestlowest = computeminmax(marks); // compute highest and lowest marks out of 30 students
        System.out.println("Highest marks obtained in " + name + " is " + highestlowest[0]);
        System.out.println("Lowest marks obtained in " + name + " is " + highestlowest[1]);
        double[] meanstd = MeanStd(marks);
@@ -27,8 +27,19 @@ public class assignment1
    public static ArrayList<Float> getStudentMarks(Scanner s){
        
    }
-   public static int[] computeminmax(ArrayList<Float> marks){
-       
+   public static float[] computeminmax(ArrayList<Float> marks){
+       float highest = 0;
+       float lowest = marks.get(0);
+       for(float mark : marks){
+           if(mark > highest){
+               highest = mark;
+           }
+           if(mark < lowest){
+               lowest = mark;
+           }
+       }
+       float [] highestlowest = {highest, lowest};
+       return highestlowest;
    }
    public static double[] MeanStd(ArrayList<Float> marks){
        
